54f55369674d64434d33f1186bd91eeff9521375
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-11-06 13:46:50 +0400

    Update settings.py

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -22,8 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
 
-    'debug_toolbar',
-
     'blog',
 ]
 
@@ -33,7 +31,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
