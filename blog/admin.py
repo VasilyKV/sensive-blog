3592b6cb1d722e3013fdd9d28156d8ceaad418fc
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-11-07 09:37:27 +0400

    fix admin pages load time

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
+class CommentAdmin(admin.ModelAdmin):
+    raw_id_fields=('post','author',)
+    list_display=('post','author', 'text',)
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields=('author','likes','tags')
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
